148097fdb8b543fb198a93ce083b32c13ca2a013
hmprt <harvey@atomicsear.ch>
2022-09-20 11:01:27 -0700

    Updated resolution

--- conway.js
+++ conway.js
@@ -1,14 +1,14 @@
 // let resolution be a random number between 10 and 70
 
-let RESOLUTION = 5
+let RESOLUTION = randint(8, 20)
 
 let CANVAS = document.getElementById('canvas');
 let FRAMERATE = 120
-let DEFAULT_LIFETIME = randint(200, 1000)
-let CHROMATIC_ABERRANCE_FACTOR = Math.random() * 0.02
+let DEFAULT_LIFETIME = randint(30, 200)
+let CHROMATIC_ABERRANCE_FACTOR = Math.random() * 0.035
 let BACKGROUND_COLOR = randomHex()
 let CELL_COLOR = randomHex()
-BACKGROUND_VOLATILITY = Math.random() * 0.025
+BACKGROUND_VOLATILITY = Math.random() * 0.02
 
 mouse_x = 0;
 mouse_y = 0;
